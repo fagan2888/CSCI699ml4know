059031fd9648fa8331841f2cf5fabc47f8ad3591
vermouth1992 <czhangseu@gmail.com>
2019-03-06 19:26:33 -0800

    environment ready

--- project/portfolio_mangement/envs/portfolio.py
+++ project/portfolio_mangement/envs/portfolio.py
@@ -2,17 +2,53 @@
 Defines the environment for portfolio management
 """
 
-import pandas as pd
-import gym
-import numpy as np
-from typing import Dict
+from collections import OrderedDict
 from pprint import pprint
 
-
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from torchlib.common import eps
 
+date_format = '%Y-%m-%d'
+
+
+def sharpe(returns, freq=30, rfr=0):
+    """ Given a set of returns, calculates naive (rfr=0) sharpe (eq 28). """
+    return (np.sqrt(freq) * np.mean(returns - rfr + eps)) / np.std(returns - rfr + eps)
+
+
+def max_drawdown(returns):
+    """ Max drawdown. See https://www.investopedia.com/terms/m/maximum-drawdown-mdd.asp """
+    peak = returns.max()
+    trough = returns[returns.idxmax():].min()
+    return (trough - peak) / (peak + eps)
+
+
+class StockObservation(object):
+    """
+    A stock observation consists of previous day numeric, current day numeric and current day news
+    """
+
+    def __init__(self, numeric, news, prev_numeric, current_date):
+        self.prev_numeric = prev_numeric
+        self.numeric = numeric
+        self.news = news
+        self.current_date = current_date
+
+    def get_close_ratio(self):
+        return self.numeric[:, -1] / self.prev_numeric[:, -1]
+
+    def get_news(self):
+        return self.news
+
+    def get_current_date(self):
+        return self.current_date
+
+
 class DataGenerator(object):
-    def __init__(self, pd_frame: Dict, total_steps=260, start_date=None):
+    def __init__(self, pd_frame: OrderedDict, total_steps=260, start_date=None):
         """
 
         Args:
@@ -30,6 +66,39 @@ class DataGenerator(object):
         self.last_record_date = pd.Timestamp(any_stock_frame.index.values[-1])
         self.news_title = ['Top{}'.format(i + 1) for i in range(25)]
 
+        if self.start_date and self.start_date not in self.date_index:
+            raise ValueError('Start date not in date index. Must from {} to {} and must be weekdays'.format(
+                self.earliest_record_date.strftime(date_format), self.last_record_date.strftime(date_format)))
+
+    def _get_data_by_date(self, date):
+        prices = []
+        news = []
+        for stock in self.pd_frame:
+            current_frame = self.pd_frame[stock]
+            line = current_frame.loc[date]
+            open = line['Open']
+            high = line['High']
+            low = line['Low']
+            close = line['Close']
+            current_price = np.array([open, high, low, close])
+            current_news = []
+            for title in self.news_title:
+                current_news.append(line[title])
+
+            prices.append(current_price)
+            news.append(current_news)
+
+        prices = np.array(prices)
+        return prices, news
+
+    def _get_next_date(self):
+        next_date = self.current_date + pd.Timedelta(days=1)
+        while next_date not in self.date_index:
+            if next_date > self.last_record_date:
+                raise ValueError('Current date {} exceeds max date {}'.format(self.current_date,
+                                                                              self.last_record_date))
+            next_date += pd.Timedelta(days=1)
+        return next_date
 
     def step(self):
         """
@@ -37,40 +106,32 @@ class DataGenerator(object):
         Returns: a data dictionary map from stock name to (open, high, low, close, Top1, ..., Top25)
 
         """
-        current_data = {}
-        for stock in self.pd_frame:
-            current_frame = self.pd_frame[stock]
-            line = current_frame.loc[self.current_date]
-            open = line['Open']
-            high = line['High']
-            low = line['Low']
-            close = line['Close']
-            price = [open, high, low, close]
-            news = []
-            for title in self.news_title:
-                news.append(line[title])
+        self.steps += 1
+        self.current_date = self._get_next_date()
+        if self.current_date == self.last_record_date or self.steps == self.total_steps:
+            done = True
+        else:
+            done = False
 
-            data = (price, news)
-            current_data[stock] = data
-
-        previous_date = self.current_date  # datetime is immutable
-
-        self.current_date += pd.Timedelta(days=1)
-        while self.current_date not in self.date_index:
-            if self.current_date > self.last_record_date:
-                raise ValueError('Current date {} exceeds max date {}'.format(self.current_date,
-                                                                              self.last_record_date))
-            self.current_date += pd.Timedelta(days=1)
-
-        return current_data, previous_date
+        current_prices, current_news = self._get_data_by_date(self.current_date)
+        obs = StockObservation(current_prices, current_news, self.prev_prices, self.current_date)
+        self.prev_prices = current_prices.copy()
+        return obs, done
 
     def reset(self):
+        self.steps = 0
         if self.start_date:
             self.current_date = self.start_date
         else:
-            idx = np.random.randint(low=0, high=self.date_index.length - self.total_steps)
-            self.current_date = self.date_index[idx]   # random select
+            idx = np.random.randint(low=0, high=self.date_index.size - self.total_steps - 1)
+            self.current_date = self.date_index[idx]  # random select
 
+        prev_prices, _ = self._get_data_by_date(self.current_date)
+        self.current_date = self._get_next_date()
+        current_prices, current_news = self._get_data_by_date(self.current_date)
+        obs = StockObservation(current_prices, current_news, prev_prices, self.current_date)
+        self.prev_prices = current_prices.copy()
+        return obs
 
 
 class PortfolioSim(object):
@@ -88,7 +149,7 @@ class PortfolioSim(object):
             e.g. [1.0, 0.9, 1.1]
         Numbered equations are from https://arxiv.org/abs/1706.10059
         """
-        assert w1.shape == y1.shape, 'w1 and y1 must have the same shape'
+        assert w1.shape == y1.shape, 'w1 and y1 must have the same shape.'
         assert y1[0] == 1.0, 'y1[0] must be 1'
 
         w0 = self.w0
@@ -145,14 +206,13 @@ class PortfolioEnv(gym.Env):
     metadata = {'render.modes': ['human', 'ansi']}
 
     def __init__(self,
-                 history,
-                 abbreviation,
-                 steps=730,  # 2 years
+                 pd_frame_dict,
+                 total_steps=260,  # 1 years
                  trading_cost=0.0025,
                  time_cost=0.00,
                  window_length=50,
                  start_idx=0,
-                 sample_start_date=None
+                 start_date=None
                  ):
         """
         An environment for financial portfolio management.
@@ -167,37 +227,34 @@ class PortfolioEnv(gym.Env):
             sample_start_date - The start date sampling from the history
         """
         self.window_length = window_length
-        self.num_stocks = history.shape[0]
+        self.num_stocks = len(pd_frame_dict.keys())
         self.start_idx = start_idx
 
-        self.src = DataGenerator(history, abbreviation, steps=steps, window_length=window_length, start_idx=start_idx,
-                                 start_date=sample_start_date)
+        self.src = DataGenerator(pd_frame_dict, total_steps=total_steps, start_date=start_date)
 
         self.sim = PortfolioSim(
-            asset_names=abbreviation,
+            asset_names=pd_frame_dict.keys(),
             trading_cost=trading_cost,
             time_cost=time_cost,
-            steps=steps)
+            total_steps=total_steps)
 
         # openai gym attributes
         # action will be the portfolio weights from 0 to 1 for each asset
         self.action_space = gym.spaces.Box(
-            0, 1, shape=(len(self.src.asset_names) + 1,), dtype=np.float32)  # include cash
-
-        # get the observation space from the data min and max
-        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(len(abbreviation), window_length,
-                                                                                 history.shape[-1]), dtype=np.float32)
+            0, 1, shape=(self.num_stocks + 1,), dtype=np.float32)  # include cash
 
     def step(self, action):
-        return self._step(action)
-
-    def _step(self, action):
         """
         Step the env.
         Actions should be portfolio [w0...]
         - Where wn is a portfolio weight from 0 to 1. The first is cash_bias
         - cn is the portfolio conversion weights see PortioSim._step for description
         """
+        if isinstance(action, list):
+            action = np.array(action)
+
+        assert isinstance(action, np.ndarray), 'Action must be a numpy array'
+
         np.testing.assert_almost_equal(
             action.shape,
             (len(self.sim.asset_names) + 1,)
@@ -214,48 +271,31 @@ class PortfolioEnv(gym.Env):
         np.testing.assert_almost_equal(
             np.sum(weights), 1.0, 3, err_msg='weights should sum to 1. action="%s"' % weights)
 
-        observation, done1, ground_truth_obs = self.src._step()
+        observation, done1 = self.src.step()
 
-        # concatenate observation with ones
-        cash_observation = np.ones((1, self.window_length, observation.shape[2]))
-        observation = np.concatenate((cash_observation, observation), axis=0)
-
-        cash_ground_truth = np.ones((1, 1, ground_truth_obs.shape[2]))
-        ground_truth_obs = np.concatenate((cash_ground_truth, ground_truth_obs), axis=0)
-
-        # relative price vector of last observation day (close/open)
-        close_price_vector = observation[:, -1, 3]
-        open_price_vector = observation[:, -1, 0]
-        y1 = close_price_vector / open_price_vector
-        reward, info, done2 = self.sim._step(weights, y1)
+        # relative price vector of last observation day (close/close)
+        y1 = observation.get_close_ratio()
+        y1 = np.insert(y1, 0, 1.)
+        reward, info, done2 = self.sim.step(weights, y1)
 
         # calculate return for buy and hold a bit of each asset
         info['market_value'] = np.cumprod([inf["return"] for inf in self.infos + [info]])[-1]
         # add dates
-        info['date'] = index_to_date(self.start_idx + self.src.idx + self.src.step)
-        info['steps'] = self.src.step
-        info['next_obs'] = ground_truth_obs
+        info['date'] = self.current_date
+        self.current_date = observation.get_current_date()
 
         self.infos.append(info)
 
         return observation, reward, done1 or done2, info
 
     def reset(self):
-        return self._reset()
-
-    def _reset(self):
         self.infos = []
         self.sim.reset()
-        observation, ground_truth_obs = self.src.reset()
-        cash_observation = np.ones((1, self.window_length, observation.shape[2]))
-        observation = np.concatenate((cash_observation, observation), axis=0)
-        cash_ground_truth = np.ones((1, 1, ground_truth_obs.shape[2]))
-        ground_truth_obs = np.concatenate((cash_ground_truth, ground_truth_obs), axis=0)
-        info = {}
-        info['next_obs'] = ground_truth_obs
-        return observation, info
+        observation = self.src.reset()
+        self.current_date = observation.get_current_date()
+        return observation
 
-    def _render(self, mode='human', close=False):
+    def render(self, mode='human', close=False):
         if close:
             return
         if mode == 'ansi':
@@ -263,15 +303,27 @@ class PortfolioEnv(gym.Env):
         elif mode == 'human':
             self.plot()
 
-    def render(self, mode='human', close=False):
-        return self._render(mode='human', close=False)
-
     def plot(self):
         # show a plot of portfolio vs mean market performance
         df_info = pd.DataFrame(self.infos)
-        df_info['date'] = pd.to_datetime(df_info['date'], format='%Y-%m-%d')
         df_info.set_index('date', inplace=True)
         mdd = max_drawdown(df_info.rate_of_return + 1)
         sharpe_ratio = sharpe(df_info.rate_of_return)
         title = 'max_drawdown={: 2.2%} sharpe_ratio={: 2.4f}'.format(mdd, sharpe_ratio)
         df_info[["portfolio_value", "market_value"]].plot(title=title, fig=plt.gcf(), rot=30)
+
+
+class PortfolioEnvPriceOnly(PortfolioEnv):
+    def step(self, action):
+        observation, reward, done, info = super(PortfolioEnvPriceOnly, self).step(action)
+        close_ratio = self.get_normalized_close_ratio(observation)
+        return close_ratio, reward, done, info
+
+    def reset(self):
+        observation = super(PortfolioEnvPriceOnly, self).reset()
+        return self.get_normalized_close_ratio(observation)
+
+    def get_normalized_close_ratio(self, observation):
+        close_ratio = observation.get_close_ratio()
+        close_ratio = (close_ratio - 1.) * 100
+        return close_ratio
--- project/portfolio_mangement/utils/data.py
+++ project/portfolio_mangement/utils/data.py
@@ -2,6 +2,8 @@
 Data utilities for import various data source and formatting
 """
 
+import os
+
 import pandas as pd
 
 
@@ -14,12 +16,21 @@ def read_djia_observation(folder_path):
     Returns: panda data frame containing (date, open, high, low, close, volume, top1, top2, ..., top25)
 
     """
-    return pd.DataFrame()
-
+    combined_csv_path = os.path.join(folder_path, 'Combined_News_DJIA.csv')
+    dija_table_csv = os.path.join(folder_path, 'DJIA_table.csv')
+    news_table = pd.read_csv(combined_csv_path)
+    djia_table = pd.read_csv(dija_table_csv)
+    assert news_table.shape[0] == djia_table.shape[0]
+    news_table = news_table.set_index(pd.DatetimeIndex(news_table['Date'])).drop('Date', axis=1)
+    djia_table = djia_table.set_index(pd.DatetimeIndex(djia_table['Date'])).drop('Date', axis=1)
+    djia_pd_frame = news_table.join(djia_table)
+    djia_pd_frame = djia_pd_frame.sort_index()
+    assert djia_pd_frame.shape[0] == djia_table.shape[0]
+    return djia_pd_frame
 
 
 def read_djia_sentiment(folder_path):
-    """ Read DJIA for training sentiment model
+    """ Read DJIA for training sentiment model. Note the label is calculated using Close price
 
     Args:
         folder_path: path to stocknews folder containing three csv files.
@@ -31,3 +42,6 @@ def read_djia_sentiment(folder_path):
     """
     return [[]], []
 
+
+if __name__ == '__main__':
+    djia_pd_frame = read_djia_observation('../../data/stocknews')
